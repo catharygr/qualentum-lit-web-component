4d522f2de80fc515b0662ab6dc30352e2b597b80
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-29 13:01:51 +0000

    Trabajando en los estilos

--- library/timer/timer-component.js
+++ library/timer/timer-component.js
@@ -1,12 +1,50 @@
 import { LitElement, html, css } from "lit";
 
 export class TimerComponent extends LitElement {
+  static styles = css`
+    .timer-container {
+      display: flex;
+      flex-direction: column;
+      margin-block: 3rem;
+      background-color: var(--text-color);
+      color: var(--neutral-color);
+      padding: 1.5rem;
+      border-radius: 0.5rem;
+    }
+    .btn-container {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      gap: 1rem;
+    }
+    button {
+      background-color: var(--primary-color);
+      padding: 0.7rem 1.8rem;
+      border: none;
+      border-radius: 0.5rem;
+      cursor: pointer;
+      font-size: 1rem;
+      transition: all 0.3s ease-in-out;
+    }
+    button:hover {
+      scale: 1.03;
+      box-shadow: 0 0 0.5rem var(--primary-color);
+    }
+  `;
   constructor() {
     super();
   }
 
   render() {
-    return html` <div>Timer</div> `;
+    return html`
+      <div class="timer-container">
+        <div class="btn-container">
+          <button>Pause</button>
+          <button>Play</button>
+          <button>Reset</button>
+        </div>
+      </div>
+    `;
   }
 }
 
