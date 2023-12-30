a253c2459a748c68df24b4e439dfb22c57584edc
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-30 12:50:15 +0000

    Trabajando en la lógica de la web-component

--- library/timer/shoppingCart-timer.js
+++ library/timer/shoppingCart-timer.js
@@ -8,6 +8,19 @@ export class ShoppingCartTimer extends LitElement {
     title: { type: String },
   };
 
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("play", this.playTimer, true);
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener("play", this.playTimer, true);
+  }
+
+  playTimer() {
+    console.log("play-hijo");
+  }
+
   render() {
     return html`
       <div>
